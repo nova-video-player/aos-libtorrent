2de260a5276b19c49fe35072c16a275ff965104b
Nicolas Noury <noury@archos.com>
2017-07-20 11:03:27 +0200

    no more armeabi

--- build-android-all.sh
+++ build-android-all.sh
@@ -3,13 +3,13 @@
 set -e
 
 found=1
-for i in armeabi x86 armeabi-v7a;do
+for i in x86 armeabi-v7a;do
 	[ ! -f bin-$i/gcc-androidR8e/release/boost-source/link-static/threading-multi/libtorrent.a ] && found=0
 done
 
 [ "$found" == 1 ] && exit 0
 
-for i in armeabi x86 armeabi-v7a;do
+for i in x86 armeabi-v7a;do
 	rm -f bin
 	mkdir -p bin-"$i"
 	ln -s bin-"$i" bin
