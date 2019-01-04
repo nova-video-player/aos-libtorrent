5df92eed65c33cb7bdc111d92f8021fc1ee79ccc
Nicolas Noury <atrus.nn@gmail.com>
2019-01-04 12:54:24 +0100

    Add build files
    and make it build on recent ndk
    
    ignore built files

---
+++ build-android-all.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+#found=1
+#for i in x86 armeabi-v7a;do
+#	[ ! -f bin-$i/gcc-androidR8e/release/boost-source/link-static/threading-multi/libtorrent.a ] && found=0
+#done
+#
+#[ "$found" == 1 ] && exit 0
+
+for i in x86 armeabi-v7a;do
+	rm -f bin
+	mkdir -p bin-"$i"
+	ln -s bin-"$i" bin
+	bash build-android.sh "$i"
+	rm -f bin
+done
