55825740854ac81d6bb393d3f62c37d3d5b99474
Nicolas Noury <atrus.nn@gmail.com>
2019-01-04 12:54:24 +0100

    Add build files
    and make it build on recent ndk
    
    ignore built files

---
+++ README.md
@@ -0,0 +1,4 @@
+### Libtorrent
+
+Libtorrent as used by [AVP](https://github.com/archos-sa/aos-AVP).
+Please see upstream project: https://github.com/arvidn/libtorrent
---
+++ build-android.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -e
+#Needed variables:
+# - $android_ndk
+# - $REPO_TOP_DIR
+# - $1 = arch
+
+export BOOST_VERSION=1_69_0
+export PlatformOS=linux
+export HOST=x86_64
+
+export ARCH=$1
+
+export AndroidNDKRoot=$android_ndk
+
+AndroidBinariesPath=""
+#toolchains/llvm/prebuilt/linux-x86_64/bin
+for i in aarch64-linux-android arm-linux-androideabi x86;do
+  AndroidBinariesPath=$AndroidBinariesPath:$AndroidNDKRoot/toolchains/llvm/prebuilt/${PlatformOS}-"${HOST}/bin/"
+done
+export PATH=$AndroidBinariesPath:$PATH
+export BOOST_ROOT=$REPO_TOP_DIR/native/newboost/boost_$BOOST_VERSION-$ARCH
+
+#cp $BOOST_ROOT/../configs/user-config-boost-$BOOST_VERSION-${ARCH}.jam user-config.jam
+#cp $BOOST_ROOT/../configs/user-config-boost-$BOOST_VERSION-${ARCH}.jam $BOOST_ROOT/tools/build/v2/user-config.jam
+export BOOST_BUILD_PATH=$PWD
+$BOOST_ROOT/tools/build/src/engine/bin.linuxx86_64/b2 toolset=clang-android link=static runtime-link=static release -j4
