1e08c6bbc5effe798d58426991e3a9fa89e59806
Marc de Courville <marc@courville.org>
2019-01-17 17:35:03 +0100

    Add macosx host...

--- build-android.sh
+++ build-android.sh
@@ -25,4 +25,8 @@ export BOOST_ROOT=$REPO_TOP_DIR/native/boost/boost_$BOOST_VERSION-$ARCH
 #cp $BOOST_ROOT/../configs/user-config-boost-$BOOST_VERSION-${ARCH}.jam user-config.jam
 #cp $BOOST_ROOT/../configs/user-config-boost-$BOOST_VERSION-${ARCH}.jam $BOOST_ROOT/tools/build/v2/user-config.jam
 export BOOST_BUILD_PATH=$PWD
-$BOOST_ROOT/tools/build/src/engine/bin.linuxx86_64/b2 toolset=clang-android link=static runtime-link=static release -j4
+
+os=linux
+[ `uname` == "Darwin" ] && os=macosx
+
+$BOOST_ROOT/tools/build/src/engine/bin.${os}x86_64/b2 toolset=clang-android link=static runtime-link=static release -j4
